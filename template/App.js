4f6f2f6e79d744d65c78111155780047b70311c0
alexandr_zavialov <alexandr.zavialov@onix-systems.com>
2021-08-01 02:41:22 +0300

    init

--- template/App.js
+++ template/App.js
@@ -9,7 +9,7 @@ export default function Main() {
 
   return (
     <Suspense fallback={<View />}>
-      <AppNavigator />
+        <AppNavigator />
     </Suspense>
-  );
+);
 }
--- template/index.js
+++ template/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-import store from './src/store/store';
+import {name as appName} from './app.json';
 
-function Root() {
-    return (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
-}
-
-AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerComponent(appName, () => App);
